36430c8d0aff0ed32073c7a8662d83ab105a2e52
Демитов Дмитрий <SRT_DemitovD@4000a.armtek.local>
2018-05-07 12:29:09 +0400

    -

--- serialport.h
+++ serialport.h
@@ -7,7 +7,7 @@
 class SerialPort : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString serial_data READ get_serial_data WRITE set_serial_data NOTIFY serial_data_Changed )
+    Q_PROPERTY(QString serial_data READ get_serial_data WRITE set_serial_data NOTIFY serial_data_Changed)
 
 public:
     SerialPort(QObject *parent = 0);
@@ -24,8 +24,8 @@ signals:
 
 private:
     QSerialPort *arduino;
-    static const quint16 arduino_uno_vendor_id = 0x2341;
-    static const quint16 arduino_uno_product_id = 0x0001;
+//    static const quint16 arduino_uno_vendor_id = 0x2341;
+//    static const quint16 arduino_uno_product_id = 0x0001;
     QString mSerial_data;
 
     void openDefault();
